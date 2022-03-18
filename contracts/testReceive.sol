c8dfdc44e43a212e07bb3d970d84eb16a6631376
yeosiowvic <victoryeo1974@gmail.com>
2022-03-18 13:27:01 +0800

    add method to send ether

--- contracts/testReceive.sol
+++ contracts/testReceive.sol
@@ -3,9 +3,19 @@
 pragma solidity ^0.8.0;
 
 contract TestSend {
- function send(address payable _receiver) external payable {
+  function send(address payable _receiver) external payable {
+    // no longer recommended to use
      _receiver.transfer(msg.value);
- }
+  }
+
+  function callSend(address payable _receiver) external payable {
+    // current recommended method to send ether
+    (bool success, ) = payable(_receiver).call{
+        value: msg.value,
+        gas: 5000
+    }("");
+    require(success, "Failed to send ether");
+  }
 }
 
 contract TestReceive {
