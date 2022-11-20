4c3a8c1b6fd83d96640257a2d150c6c85969a0be
yeosiowvic <victoryeo1974@gmail.com>
2022-11-20 13:19:32 +0800

    add method to call fallback function

--- contracts/testReceive.sol
+++ contracts/testReceive.sol
@@ -16,11 +16,30 @@ contract TestSend {
     }("");
     require(success, "Failed to send ether");
   }
+
+  // this function will call fallback()
+  //         is msg.data empty?
+  //            /        \
+  //           yes        no
+  //          /             \
+  //  receive() exists?    is the function selector there?
+  //      /  \             /      \
+  //    yes   no          no      yes
+  //    /       \        /          \
+  // receive()  fallback()        function_to_call()
+  function callFallback(address payable _receiver) external payable {
+    (bool success, ) = payable(_receiver).call{
+        value: msg.value,
+        gas: 5000
+    }(abi.encodeWithSignature('myfunction(uint)', 400));  //pass in msg.data
+    require(success, "Failed to send ether");
+  }
 }
 
 contract TestReceive {
     event Deployed(address addr);
     event Received(address, uint);
+    event Fallbacked(address, uint);
     uint y;
 
     constructor() {
@@ -31,6 +50,7 @@ contract TestReceive {
     }
 
     fallback() external payable { 
+        emit Fallbacked(msg.sender, msg.value);
         y = msg.value; 
     }
 }
